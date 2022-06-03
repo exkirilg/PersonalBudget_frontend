484925db1f4f706c4ee4437fd043c316cd5af534
Kirill <62622495+exkirilg@users.noreply.github.com>
2022-06-03 22:31:00 +0300

    modify budget operations table

--- src/Components/BudgetOperationsTable.tsx
+++ src/Components/BudgetOperationsTable.tsx
@@ -1,3 +1,4 @@
+import { Console } from "console";
 import React from "react";
 
 import Container from "react-bootstrap/Container";
@@ -6,7 +7,6 @@ import Stack from "react-bootstrap/Stack";
 import Table from "react-bootstrap/Table";
 
 import { BudgetOperation } from "../Interfaces/BudgetOperation";
-import { BudgetOperationTableRow } from "./BudgetOperationTableRow";
 
 interface Props {
     data: BudgetOperation[];
@@ -36,10 +36,49 @@ export const BudgetOperationsTable = ({ data }: Props) => (
             </thead>
             <tbody>
                 {data.map((operation) => (
-                    <BudgetOperationTableRow operation={operation} />
+                    budgetOperationsTableRow(operation)
                 ))}
             </tbody>
+            <tfoot>
+                <tr>
+                    <td className="fs-5 fw-bold text-end" colSpan={3}>
+                        {budgetOperationsTableResult(getBudgetOperationsResult(data))}
+                    </td>
+                </tr>
+            </tfoot>
         </Table>
 
     </Container>
 );
+
+const budgetOperationsTableRow = (operation : BudgetOperation) => {
+    let bgClass = operation.type === 0 ? "table-success" : "table-danger";
+    return (
+        <tr className={bgClass}>
+            <td>{operation.date.toLocaleString()}</td>
+            <td>{operation.item.name}</td>
+            <td>{operation.sum}</td>
+        </tr>
+    );
+}
+
+const budgetOperationsTableResult = (result: number) => {
+    let fontClass = result >= 0 ? "text-success" : "text-danger";
+    return (
+        <span className={fontClass}>
+            {result.toFixed(2)}
+        </span>
+    );
+}
+
+function getBudgetOperationsResult(data : BudgetOperation[]) : number {
+    return getBudgetOperationsSum(data, 0) - getBudgetOperationsSum(data, 1);
+}
+
+function getBudgetOperationsSum (data : BudgetOperation[], type : number) : number {
+    let result = data.reduce((a, b) => {
+        a += (b.type === type ? b.sum : 0);
+        return a;
+    }, 0);
+    return result;
+}
